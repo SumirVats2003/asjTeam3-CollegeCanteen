956e0d37ca3f2082fe4845bb32a8295e4ba42c06
Sumir Vats <65482161+SumirVats2003@users.noreply.github.com>
2022-01-02 11:14:59 +0000

    Add files via upload

---
+++ app/src/main/java/com/example/collegecanteen_otp/CollegeCanteenOTP.kt
@@ -0,0 +1,11 @@
+package com.example.collegecanteen_otp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CollegeCanteenOTP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_college_canteen_otp)
+    }
+}
---
+++ app/src/main/java/com/example/collegecanteen_otp/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.collegecanteen_otp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import java.lang.Double.parseDouble
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val image_view = findViewById(R.id.sendOtp) as ImageView
+
+        fun nextActivity() {
+            image_view.setOnClickListener {
+                val intent = Intent(this, CollegeCanteenOTP::class.java)
+                startActivity(intent)
+            }
+        }
+
+        fun checkPhone(view: android.view.View) {
+            var phone = image_view.toString()
+            var numeric = true
+            if(phone.length == 10) {
+
+                try {
+                    val num = parseDouble(phone)
+                } catch (e: NumberFormatException) {
+                    numeric = false
+                }
+            }
+            if (numeric) {
+                nextActivity()
+            }
+
+        }
+    }
+}
